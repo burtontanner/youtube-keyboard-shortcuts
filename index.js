5d69ad7592aab61838cbbf1c79b92ce127aa15b1
burtontanner <tburt713@yahoo.com>
2020-01-06 14:35:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,34 @@
  */
 
 function simulate(element, eventName) {
-    var options = extend(defaultOptions, arguments[2] || {});
-    var oEvent, eventType = null;
-    for (var name in eventMatchers){
-        if (eventMatchers[name].test(eventName)) { eventType = name; break; }
-    }
-    if (!eventType)
-        throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+	try{
+	    var options = extend(defaultOptions, arguments[2] || {});
+	    var oEvent, eventType = null;
+	    for (var name in eventMatchers){
+		if (eventMatchers[name].test(eventName)) { eventType = name; break; }
+	    }
+	    if (!eventType)
+		throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
 
-    if (document.createEvent){
-        oEvent = document.createEvent(eventType);
-        if (eventType == 'HTMLEvents'){
-            oEvent.initEvent(eventName, options.bubbles, options.cancelable);
-        } else {
-            oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
-            options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
-            options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
-        }
-        element.dispatchEvent(oEvent);
-    } else {
-        options.clientX = options.pointerX;
-        options.clientY = options.pointerY;
-        var evt = document.createEventObject();
-        oEvent = extend(evt, options);
-        element.fireEvent('on' + eventName, oEvent);
-    }
-    return element;
+	    if (document.createEvent){
+		oEvent = document.createEvent(eventType);
+		if (eventType == 'HTMLEvents'){
+		    oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+		} else {
+		    oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+		    options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+		    options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+		}
+		element.dispatchEvent(oEvent);
+	    } else {
+		options.clientX = options.pointerX;
+		options.clientY = options.pointerY;
+		var evt = document.createEventObject();
+		oEvent = extend(evt, options);
+		element.fireEvent('on' + eventName, oEvent);
+	    }
+	    return element;
+	}catch(e){console.error(e));
 }
 function extend(destination, source) {
     for (var property in source)
